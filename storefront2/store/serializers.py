327b440682e3daf238829cf3e9e07e83184ae230
JunfengZhao2357 <59008306+JunfengZhao2357@users.noreply.github.com>
2024-09-27 14:19:32 -0700

    Use model serializer to remove repeated code
    Signed-off-by: JunfengZhao2357 <59008306+JunfengZhao2357@users.noreply.github.com>

--- storefront2/store/serializers.py
+++ storefront2/store/serializers.py
@@ -3,24 +3,30 @@ from rest_framework import serializers
 from store.models import Product, Collection
 
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class CollectionSerializer(serializers.ModelSerializer):
+    # id = serializers.IntegerField()
+    # title = serializers.CharField(max_length=255)
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
 
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2, source="unit_price")
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price', 'price_with_tax', 'collection']
+    # id = serializers.IntegerField()
+    # title = serializers.CharField(max_length=255)
+    # price = serializers.DecimalField(max_digits=6, decimal_places=2, source="unit_price")
     price_with_tax = serializers.SerializerMethodField(method_name="calculate_tax")
     # Collection model has __str__ method, so it can return a string
     # collection = serializers.StringRelatedField()
     # Nested objects
     # collection = CollectionSerializer()
     # if we want to generate a HTTP link
-    collection = serializers.HyperlinkedRelatedField(
-        queryset = Collection.objects.all(),
-        view_name = 'collection-detail'
-    )
+    # collection = serializers.HyperlinkedRelatedField(
+    #     queryset = Collection.objects.all(),
+    #     view_name = 'collection-detail'
+    # )
     def calculate_tax(self, product: Product):
         return product.unit_price*Decimal(1.1)
