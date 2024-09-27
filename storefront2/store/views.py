42037cb54bf3fbfb22d7643d86666e7631f5e5d3
JunfengZhao2357 <59008306+JunfengZhao2357@users.noreply.github.com>
2024-09-27 11:50:49 -0700

    Create Serializers and Serializing porjects
    Signed-off-by: JunfengZhao2357 <59008306+JunfengZhao2357@users.noreply.github.com>

--- storefront2/store/views.py
+++ storefront2/store/views.py
@@ -1,13 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .models import Product
+from .serializers import ProductSerializer
 
 
 @api_view()
 def product_list(request):
-    return Response('OK')
+    queryset = Product.objects.all()
+    # many=true is to tell serializer to convert the whole queryset one by one to dict
+    serializer = ProductSerializer(queryset, many=True)
+    return Response(serializer.data)
 
 @api_view()
 def product_detail(request, id):
-    return Response(id)
+    try: 
+        product = Product.objects.get(pk=id)
+        # serializer will convert our product to dictionary
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
